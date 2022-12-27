760b549e72d1d3cfbc42ff847bae5cac20facce6
Laurenz-Guevara <laurenzguevara@outlook.com>
2022-12-27 01:45:32 +0000

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the home page.</h1>
+      <h1>Welcome to the home page!</h1>
       <Link to="/about">
         <h1>About</h1>
       </Link>
